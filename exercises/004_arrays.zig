af340e24413987343613319ab600f6ab753d899f
ayawen01 <1715281438@qq.com>
2023-03-15 19:49:43 +0800

    update format...

--- exercises/004_arrays.zig
+++ exercises/004_arrays.zig
@@ -3,20 +3,20 @@
 //
 //   var foo: [3]u32 = [3]u32{ 42, 108, 5423 };
 //
-// 当 Zig 可以推断出数组的大小时，您可以用'_'表示大小。
-// 您也可以让 Zig 推断出值的类型，这样声明就不那么冗长了。
+// 当Zig可以推断出数组的大小时，您可以用'_'表示大小。
+// 您也可以让Zig推断出值的类型，这样声明就不那么冗长了。
 //
 //   var foo = [_]u32{ 42, 108, 5423 };
 //
-// 使用 array[index] 表示法获取数组的值：
+// 使用array[index]表示法获取数组的值：
 //
 //     const bar = foo[2]; // 5423
 //
-// 使用 array[index] 表示法设置数组的值：
+// 使用array[index]表示法设置数组的值：
 //
 //     foo[2] = 16;
 //
-// 使用 len 属性获取数组的长度：
+// 使用len属性获取数组的长度：
 //
 //     const length = foo.len;
 //
@@ -38,11 +38,11 @@ pub fn main() void {
 
     // (问题2)
     // 看起来我们需要完成这个表达式。用上面的例子
-    // 来把“fourth”设置为 some_primes 数组的第四个元素：
+    // 来把“fourth”设置为some_primes数组的第四个元素：
     const fourth = some_primes[3];
 
     // (问题3)
-    // 用 len 属性来获取数组的长度：
+    // 用len属性来获取数组的长度：
     const length = some_primes.len;
 
     std.debug.print("First: {}, Fourth: {}, Length: {}\n", .{
