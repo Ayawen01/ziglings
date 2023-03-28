8273f2e873a82c984969a0f0b9ee8ab1caa24504
ayawen01 <1715281438@qq.com>
2023-03-28 21:05:05 +0800

    065

--- exercises/064_builtins.zig
+++ exercises/064_builtins.zig
@@ -1,84 +1,70 @@
 //
-// The Zig compiler provides "builtin" functions. You've already
-// gotten used to seeing an @import() at the top of every
-// Ziglings exercise.
+// Zig 编译器提供了一些“内置”函数。你已经习惯了在每个 Ziglings 练习的顶部看到一个 @import()。
 //
-// We've also seen @intCast() in "016_for2.zig", "058_quiz7.zig";
-// and @enumToInt() in "036_enums2.zig".
+// 我们还在 "016_for2.zig", "058_quiz7.zig" 中看到了 @intCast()；
+// 在 "036_enums2.zig" 中看到了 @enumToInt()。
 //
-// Builtins are special because they are intrinsic to the Zig
-// language itself (as opposed to being provided in the standard
-// library). They are also special because they can provide
-// functionality that is only possible with help from the
-// compiler, such as type introspection (the ability to examine
-// type properties from within a program).
+// 内置函数很特别，因为它们是 Zig 语言本身的一部分（而不是由标准库提供的）。
+// 它们也很特别，因为它们可以提供一些只有编译器才能帮助实现的功能，
+// 比如类型内省（从程序中检查类型属性的能力）。
 //
-// Zig currently contains 101 builtin functions. We're certainly
-// not going to cover them all, but we can look at some
-// interesting ones.
+// Zig 目前包含了 101 个内置函数。我们当然不会涵盖它们所有的，
+// 但我们可以看一些有趣的。
 //
-// Before we begin, know that many builtin functions have
-// parameters marked as "comptime". It's probably fairly clear
-// what we mean when we say that these parameters need to be
-// "known at compile time." But rest assured we'll be doing the
-// "comptime" subject real justice soon.
+// 在我们开始之前，要知道很多内置函数有标记为“comptime”的参数。
+// 当我们说这些参数需要在编译时就已知时，我们的意思可能很清楚。
+// 但请放心，我们很快就会对“comptime”主题做一个真正的公正处理。
 //
 const print = @import("std").debug.print;
 
 pub fn main() void {
-    // The second builtin, alphabetically, is:
+    // 按字母顺序排列，第二个内置函数是：
     //   @addWithOverflow(a: anytype, b: anytype) struct { @TypeOf(a, b), u1 }
-    //     * 'T' will be the type of the other parameters.
-    //     * 'a' and 'b' are numbers of the type T.
-    //     * The return value is a tuple with the result and a possible overflow bit.
+    //     * 'T' 是其他参数的类型。
+    //     * 'a' 和 'b' 是类型 T 的数字。
+    //     * 返回值是一个元组，包含结果和可能的溢出位。
     //
-    // Let's try it with a tiny 4-bit integer size to make it clear:
+    // 让我们用一个很小的 4 位整数大小来试一试，以便清楚地看到：
     const a: u4 = 0b1101;
     const b: u4 = 0b0101;
     const my_result = @addWithOverflow(a, b);
 
-    // Check out our fancy formatting! b:0>4 means, "print
-    // as a binary number, zero-pad right-aligned four digits."
-    // The print() below will produce: "1101 + 0101 = 0010 (true)".
+    // 查看我们的精美格式！b:0>4表示，“以二进制数打印，右对齐四位数，用0填充”。
+    // 下面的print()将产生：“1101 + 0101 = 0010 (true)”。
     print("{b:0>4} + {b:0>4} = {b:0>4} ({s})", .{ a, b, my_result[0], if (my_result[1] == 1) "true" else "false" });
 
-    // Let's make sense of this answer. The value of 'b' in decimal is 5.
-    // Let's add 5 to 'a' but go one by one and see where it overflows:
+    // 让我们理解一下这个答案。'b'的十进制值是5。
+    // 让我们一步一步地把5加到'a'上，看看哪里会溢出：
     //
     //   a  |  b   | result | overflowed?
     // ----------------------------------
     // 1101 + 0001 =  1110  | false
     // 1110 + 0001 =  1111  | false
-    // 1111 + 0001 =  0000  | true  (the real answer is 10000)
+    // 1111 + 0001 =  0000  | true  (真正的答案是10000)
     // 0000 + 0001 =  0001  | false
     // 0001 + 0001 =  0010  | false
     //
-    // In the last two lines the value of 'a' is corrupted because there was
-    // an overflow in line 3, but the operations of lines 4 and 5 themselves
-    // do not overflow.
-    // There is a difference between
-    //  - a value, that overflowed at some point and is now corrupted
-    //  - a single operation that overflows and maybe causes subsequent errors
-    // In practise we usually notice the overflowed value first and have to work
-    // our way backwards to the operation that caused the overflow.
+    // 在最后两行中，'a'的值被破坏了，因为在第3行有一个溢出，但是第4行和第5行的操作本身没有溢出。
+    // 这里有一个区别：
+    // - 在某个时刻溢出并且现在已经损坏的值
+    // - 单个操作溢出并可能导致后续错误
+    // 在实践中，我们通常先注意到溢出的值，然后再回溯到导致溢出的操作。
     //
-    // If there was no overflow at all while adding 5 to a, what value would
-    // 'my_result' hold? Write the answer in into 'expected_result'.
-    const expected_result: u8 = ???;
+    // 如果在把5加到a时没有发生任何溢出，'my_result'会持有什么值？
+    // 把答案写到'expected_result'中。
+    const expected_result: u8 = 0b10010;
     print(". Without overflow: {b:0>8}. ", .{expected_result});
 
     print("Furthermore, ", .{});
 
-    // Here's a fun one:
+    // 这是一个有趣的：
     //
     //   @bitReverse(integer: anytype) T
-    //     * 'integer' is the value to reverse.
-    //     * The return value will be the same type with the
-    //       value's bits reversed!
+    //     * 'integer'是要反转的值。
+    //     * 返回值将是相同类型的反转后的值！
     //
-    // Now it's your turn. See if you can fix this attempt to use
-    // this builtin to reverse the bits of a u8 integer.
+    // 现在轮到你了。看看你能否修复这个尝试使用这个内置函数来反转一个u8整数的位。
     const input: u8 = 0b11110000;
-    const tupni: u8 = @bitReverse(input, tupni);
+    const tupni: u8 = @bitReverse(input);
     print("{b:0>8} backwards is {b:0>8}.\n", .{ input, tupni });
 }
--- exercises/065_builtins2.zig
+++ exercises/065_builtins2.zig
@@ -1,51 +1,43 @@
 //
-// Zig has builtins for mathematical operations such as...
+// Zig有一些内置的数学运算，比如...
 //
 //      @sqrt        @sin          @cos
 //      @exp         @log          @floor
 //
-// ...and lots of type casting operations such as...
+// ...还有很多类型转换的操作，比如...
 //
 //      @as          @intToError   @intToFloat
 //      @intToPtr    @ptrToInt     @enumToInt
 //
-// Spending part of a rainy day skimming through the complete
-// list of builtins in the official Zig documentation wouldn't be
-// a bad use of your time. There are some seriously cool features
-// in there. Check out @call, @compileLog, @embedFile, and @src!
+// 在雨天花一点时间浏览一下官方Zig文档中的内置函数列表也不是
+// 一件坏事。里面有一些非常酷的特性。
+// 看看@call, @compileLog,@embedFile, 和@src!
 //
 //                            ...
 //
-// For now, we're going to complete our examination of builtins
-// by exploring just THREE of Zig's MANY introspection abilities:
+// 现在，我们要完成对内置函数的探索，只看Zig的许多自省能力中的三个：
 //
 // 1. @This() type
 //
-// Returns the innermost struct, enum, or union that a function
-// call is inside.
+// 返回一个函数调用所在的最内层的结构体，枚举或联合体。
 //
 // 2. @typeInfo(comptime T: type) @import("std").builtin.TypeInfo
 //
-// Returns information about any type in a TypeInfo union which
-// will contain different information depending on which type
-// you're examining.
+// 返回一个TypeInfo联合体，包含了任何类型的信息，这个联合体会根据你检查的类型包含不同的信息。
 //
 // 3. @TypeOf(...) type
 //
-// Returns the type common to all input parameters (each of which
-// may be any expression). The type is resolved using the same
-// "peer type resolution" process the compiler itself uses when
-// inferring types.
+// 返回所有输入参数（每个都可以是任何表达式）的公共类型。这个类型是
+// 用和编译器自身使用的“同级类型解析”过程来确定的。
 //
-// (Notice how the two functions which return types start with
-// uppercase letters? This is a standard naming practice in Zig.)
+// (注意到返回类型的两个函数都以大写字母开头了吗？这是Zig中的一个标准命名惯例。)
 //
 const print = @import("std").debug.print;
 
 const Narcissus = struct {
     me: *Narcissus = undefined,
     myself: *Narcissus = undefined,
-    echo: void = undefined, // Alas, poor Echo!
+    echo: void = undefined, // 可怜的Echo!
 
     fn fetchTheMostBeautifulType() type {
         return @This();
@@ -55,48 +47,42 @@ const Narcissus = struct {
 pub fn main() void {
     var narcissus: Narcissus = Narcissus{};
 
-    // Oops! We cannot leave the 'me' and 'myself' fields
-    // undefined. Please set them here:
+    // 哎呀！我们不能把'me'和'myself'字段留空。请在这里设置它们：
     narcissus.me = &narcissus;
-    narcissus.??? = ???;
+    narcissus.myself = &narcissus;
 
-    // This determines a "peer type" from three separate
-    // references (they just happen to all be the same object).
+    // 这里根据三个不同的引用（它们恰好都是同一个对象）确定了一个“同级类型”。
     const Type1 = @TypeOf(narcissus, narcissus.me.*, narcissus.myself.*);
 
-    // Oh dear, we seem to have done something wrong when calling
-    // this function. We called it as a method, which would work
-    // if it had a self parameter. But it doesn't. (See above.)
+    // 哦，亲爱的，我们在调用这个函数时似乎做错了什么。我们把它当作一个方法调用，
+    // 这样做可以，如果它有一个self参数的话。但它没有。（看上面。）
     //
-    // The fix for this is very subtle, but it makes a big
-    // difference!
-    const Type2 = narcissus.fetchTheMostBeautifulType();
+    // 这里修复的方法很微妙，但是会有很大的区别！
+    const Type2 = Narcissus.fetchTheMostBeautifulType();
 
-    // Now we print a pithy statement about Narcissus.
+    // 现在我们打印一个关于Narcissus的简短陈述。
     print("A {s} loves all {s}es. ", .{
         maximumNarcissism(Type1),
         maximumNarcissism(Type2),
     });
 
-    //   His final words as he was looking in
-    //   those waters he habitually watched
-    //   were these:
-    //       "Alas, my beloved boy, in vain!"
-    //   The place gave every word back in reply.
-    //   He cried:
-    //            "Farewell."
-    //   And Echo called:
-    //                   "Farewell!"
+    //   他在看着那些他习惯看的水时说了这些话：
+    //       “哎呀，我心爱的男孩，白费了！”
+    //   这个地方把每个单词都回复了。
+    //   他喊道：
+    //            “再见。”
+    //   回声叫道：
+    //                   “再见！”
     //
-    //     --Ovid, The Metamorphoses
-    //       translated by Ian Johnston
+    //     --Ovid，《变形记》
+    //       由Ian Johnston翻译
 
     print("He has room in his heart for:", .{});
 
-    // A StructFields array
+    // 一个StructFields数组
     const fields = @typeInfo(Narcissus).Struct.fields;
 
-    // 'fields' is a slice of StructFields. Here's the declaration:
+    // 'fields'是StructFields的一个切片。这是声明：
     //
     //     pub const StructField = struct {
     //         name: []const u8,
@@ -106,44 +92,36 @@ pub fn main() void {
     //         alignment: comptime_int,
     //     };
     //
-    // Please complete these 'if' statements so that the field
-    // name will not be printed if the field is of type 'void'
-    // (which is a zero-bit type that takes up no space at all!):
-    if (fields[0].??? != void) {
+    // 请完成这些'if'语句，以便如果字段是'void'类型（这是一种零位类型，根本不占用空间！），则不会打印字段名：
+    if (fields[0].type != void) {
         print(" {s}", .{@typeInfo(Narcissus).Struct.fields[0].name});
     }
 
-    if (fields[1].??? != void) {
+    if (fields[1].type != void) {
         print(" {s}", .{@typeInfo(Narcissus).Struct.fields[1].name});
     }
 
-    if (fields[2].??? != void) {
+    if (fields[2].type != void) {
         print(" {s}", .{@typeInfo(Narcissus).Struct.fields[2].name});
     }
 
-    // Yuck, look at all that repeated code above! I don't know
-    // about you, but it makes me itchy.
+    // 呸，看看上面那些重复的代码！我不知道你怎么想，但它让我感到痒痒的。
     //
-    // Alas, we can't use a regular 'for' loop here because
-    // 'fields' can only be evaluated at compile time.  It seems
-    // like we're overdue to learn about this "comptime" stuff,
-    // doesn't it? Don't worry, we'll get there.
+    // 不幸的是，我们不能在这里使用常规的“for”循环，
+    // 因为'fields'只能在编译时计算。
+    // 看来我们已经过期了，需要了解一下“comptime”这个东西，不是吗？别担心，我们会到那里去的。
 
     print(".\n", .{});
 }
 
-// NOTE: This exercise did not originally include the function below.
-// But a change after Zig 0.10.0 added the source file name to the
-// type. "Narcissus" became "065_builtins2.Narcissus".
+// 注意：此练习最初未包括下面的函数。
+// 但是，在Zig 0.10.0之后进行更改时将源文件名添加到类型中。 “Narcissus"变成了"065_builtins2.Narcissus”。
 //
-// To fix this, I've added this function to strip the filename from
-// the front of the type name in the dumbest way possible. (It returns
-// a slice of the type name starting at character 14 (assuming
-// single-byte characters).
+// 为了解决这个问题，我添加了此函数以从类型名称中最愚蠢的方式删除文件名。（它返回从第14个字符（假设单字节字符）开始的类型名称切片。
 //
-// We'll be seeing @typeName again in Exercise 070. For now, you can
-// see that it takes a Type and returns a u8 "string".
+// 我们将在练习070中再次看到@typeName。
+// 现在您可以看到它需要一个Type并返回一个u8“字符串”。
 fn maximumNarcissism(myType: anytype) []const u8 {
-    // Turn '065_builtins2.Narcissus' into 'Narcissus'
+    // 将’065_builtins2.Narcissus’转换为’Narcissus’。
     return @typeName(myType)[14..];
 }
