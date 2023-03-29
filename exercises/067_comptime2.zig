1df6bd5c60ad50642dda8148f217ddbbba7a42a0
ayawen01 <1715281438@qq.com>
2023-03-29 19:55:53 +0800

    067

--- exercises/067_comptime2.zig
+++ exercises/067_comptime2.zig
@@ -1,41 +1,30 @@
 //
-// We've seen that Zig implicitly performs some evaluations at
-// compile time. But sometimes you'll want to explicitly request
-// compile time evaluation. For that, we have a new keyword:
-//
+// 我们已经看到Zig在编译时隐式执行了一些评估。但有时您需要显式请求编译时评估。为此，我们有一个新关键字：
+
 //  .     .   .      o       .          .       *  . .     .
 //    .  *  |     .    .            .   .     .   .     * .    .
 //        --o--            comptime        *    |      ..    .
 //     *    |       *  .        .    .   .    --*--  .     *  .
-//  .     .    .    .   . . .      .        .   |   .    .  .
-//
-// When placed before a variable declaration, 'comptime'
-// guarantees that every usage of that variable will be performed
-// at compile time.
-//
-// As a simple example, compare these two statements:
-//
-//    var bar1 = 5;            // ERROR!
-//    comptime var bar2 = 5;   // OKAY!
-//
-// The first one gives us an error because Zig assumes mutable
-// identifiers (declared with 'var') will be used at runtime and
-// we have not assigned a runtime type (like u8 or f32). Trying
-// to use a comptime_int of undetermined size at runtime is
-// a MEMORY CRIME and you are UNDER ARREST.
-//
-// The second one is okay because we've told Zig that 'bar2' is
-// a compile time variable. Zig will help us ensure this is true
-// and let us know if we make a mistake.
+//  .     .    .    .   . . .      .        .   |   .    .
+
+// 当放置在变量声明之前时，“comptime”保证该变量的每个使用都将在编译时执行。
+
+// 作为一个简单的例子，请比较这两个语句：
+
+// var bar1 = 5; // 错误！
+// comptime var bar2 = 5; // 没问题！
+
+// 第一个会给我们一个错误，因为Zig假定可变标识符（使用“var”声明）将在运行时使用，而我们没有分配运行时类型（如u8或f32）。尝试在运行时使用未确定大小的comptime_int是一种内存犯罪，你被逮捕了。
+
+// 第二个没问题，因为我们告诉Zig“bar2”是一个编译时变量。Zig将帮助我们确保这是正确的，并让我们知道如果我们犯了错误。
 //
 const print = @import("std").debug.print;
 
 pub fn main() void {
     //
-    // In this contrived example, we've decided to allocate some
-    // arrays using a variable count! But something's missing...
+    // 在这个人为的例子中，我们决定使用变量计数来分配一些数组！但是还缺少一些东西...
     //
-    var count = 0;
+    comptime var count = 0;
 
     count += 1;
     var a1: [count]u8 = .{'A'} ** count;
@@ -51,14 +40,11 @@ pub fn main() void {
 
     print("{s} {s} {s} {s}\n", .{ a1, a2, a3, a4 });
 
-    // Builtin BONUS!
+    // 内置奖励！
     //
-    // The @compileLog() builtin is like a print statement that
-    // ONLY operates at compile time. The Zig compiler treats
-    // @compileLog() calls as errors, so you'll want to use them
-    // temporarily to debug compile time logic.
+    // @compileLog()内置函数类似于仅在编译时操作的打印语句。
+    // Zig编译器将@compileLog()调用视为错误，因此您需要暂时使用它们来调试编译时逻辑。
     //
-    // Try uncommenting this line and playing around with it
-    // (copy it, move it) to see what it does:
-    //@compileLog("Count at compile time: ", count);
+    // 尝试取消注释此行并玩弄它（复制它，移动它）以查看它的作用：
+    // @compileLog("Count at compile time: ", count);
 }
