98142d53a06b9539fbf34df2ef0316ef43fb7224
ayawen01 <1715281438@qq.com>
2023-03-30 17:31:25 +0800

    069

--- exercises/068_comptime3.zig
+++ exercises/068_comptime3.zig
@@ -1,25 +1,17 @@
 //
-// You can also put 'comptime' before a function parameter to
-// enforce that the argument passed to the function must be known
-// at compile time. We've actually been using a function like
-// this the entire time, std.debug.print():
+// 您还可以在函数参数前面放置'comptime'来强制要求传递给函数的参数必须在编译时已知。
+// 我们实际上一直在使用这样的函数，std.debug.print()：
 //
 //     fn print(comptime fmt: []const u8, args: anytype) void
 //
-// Notice that the format string parameter 'fmt' is marked as
-// 'comptime'.  One of the neat benefits of this is that the
-// format string can be checked for errors at compile time rather
-// than crashing at runtime.
+// 请注意，格式字符串参数'fmt'标记为'comptime'。
+// 这样做的一个很好的好处是，可以在编译时检查格式字符串是否有误，而不是在运行时崩溃。
 //
-// (The actual formatting is done by std.fmt.format() and it
-// contains a complete format string parser that runs entirely at
-// compile time!)
+// （实际的格式化是由std.fmt.format()完成的，它包含一个完整的格式字符串解析器，完全在编译时运行！）
 //
 const print = @import("std").debug.print;
 
-// This struct is the model of a model boat. We can transform it
-// to any scale we would like: 1:2 is half-size, 1:32 is
-// thirty-two times smaller than the real thing, and so forth.
+// 这个结构体是模型船的模型。我们可以将它转换为任何比例尺：1:2是半尺寸，1:32比真实物品小三十二倍，等等。
 const Schooner = struct {
     name: []const u8,
     scale: u32 = 1,
@@ -30,20 +22,12 @@ const Schooner = struct {
     fn scaleMe(self: *Schooner, comptime scale: u32) void {
         var my_scale = scale;
 
-        // We did something neat here: we've anticipated the
-        // possibility of accidentally attempting to create a
-        // scale of 1:0. Rather than having this result in a
-        // divide-by-zero error at runtime, we've turned this
-        // into a compile error.
+        // 我们做了一些很棒的事情：我们预见到了意外尝试创建1:0比例尺的可能性。我们没有让它在运行时导致除以零错误，而是将其转换为编译错误。
         //
-        // This is probably the correct solution most of the
-        // time. But our model boat model program is very casual
-        // and we just want it to "do what I mean" and keep
-        // working.
+        // 这可能大多数情况下都是正确的解决方案。但是我们的模型船模型程序非常随意，我们只想让它“做我想要的”并继续工作。
         //
-        // Please change this so that it sets a 0 scale to 1
-        // instead.
-        if (my_scale == 0) @compileError("Scale 1:0 is not valid!");
+        // 请更改此设置，以便将0比例设置为1。
+        if (my_scale == 0) my_scale = 1;
 
         self.scale = my_scale;
         self.hull_length /= my_scale;
@@ -66,43 +50,37 @@ pub fn main() void {
     var shark = Schooner{ .name = "Shark" };
     var minnow = Schooner{ .name = "Minnow" };
 
-    // Hey, we can't just pass this runtime variable as an
-    // argument to the scaleMe() method. What would let us do
-    // that?
-    var scale: u32 = undefined;
+    // 嘿，我们不能只将此运行时变量作为参数传递给scaleMe()方法。什么可以让我们这样做？
+    comptime var scale: u32 = undefined;
 
-    scale = 32; // 1:32 scale
+    scale = 32; // 1:32 比例尺
 
     minnow.scaleMe(scale);
     minnow.printMe();
 
-    scale -= 16; // 1:16 scale
+    scale -= 16; // 1:16 比例尺
 
     shark.scaleMe(scale);
     shark.printMe();
 
-    scale -= 16; // 1:0 scale (oops, but DON'T FIX THIS!)
+    scale -= 16; // 1:0 比例尺（糟糕了，但不要修复它！）
 
     whale.scaleMe(scale);
     whale.printMe();
 }
 //
-// Going deeper:
+// 更深入地探讨：
 //
-// What would happen if you DID attempt to build a model in the
-// scale of 1:0?
+// 如果您尝试按比例1：0建立模型，会发生什么？
 //
-//    A) You're already done!
-//    B) You would suffer a mental divide-by-zero error.
-//    C) You would construct a singularity and destroy the
-//       planet.
+// A）您已经完成了！
+// B）您将遭受精神除以零的错误。
+// C）您将构建奇点并摧毁行星。
 //
-// And how about a model in the scale of 0:1?
+// 那么比例为0：1的模型呢？
 //
-//    A) You're already done!
-//    B) You'd arrange nothing carefully into the form of the
-//       original nothing but infinitely larger.
-//    C) You would construct a singularity and destroy the
-//       planet.
+// A）您已经完成了！
+// B）您会仔细安排无形物体，使其形成原始无形物体，但是无限大。
+// C）您将构建奇点并摧毁行星。
 //
-// Answers can be found on the back of the Ziglings packaging.
+// 答案可以在Ziglings包装的背面找到。
--- exercises/069_comptime4.zig
+++ exercises/069_comptime4.zig
@@ -1,49 +1,37 @@
 //
-// One of the more common uses of 'comptime' function parameters is
-// passing a type to a function:
+// 'comptime'函数参数的更常见用法之一是将类型传递给函数：
 //
 //     fn foo(comptime MyType: type) void { ... }
 //
-// In fact, types are ONLY available at compile time, so the
-// 'comptime' keyword is required here.
+// 实际上，类型仅在编译时可用，因此此处需要“comptime”关键字。
 //
-// Please take a moment to put on the wizard hat which has been
-// provided for you. We're about to use this ability to implement
-// a generic function.
+// 请花点时间戴上为您提供的巫师帽。我们将使用此功能来实现通用函数。
 //
 const print = @import("std").debug.print;
 
 pub fn main() void {
-    // Here we declare arrays of three different types and sizes
-    // at compile time from a function call. Neat!
-    const s1 = makeSequence(u8, 3);  // creates a [3]u8
-    const s2 = makeSequence(u32, 5); // creates a [5]u32
-    const s3 = makeSequence(i64, 7); // creates a [7]i64
+    // 在这里，我们从函数调用中在编译时声明了三种不同类型和大小的数组。很棒！
+    const s1 = makeSequence(u8, 3); // 创建一个[3]u8
+    const s2 = makeSequence(u32, 5); // 创建一个[5]u32
+    const s3 = makeSequence(i64, 7); // 创建一个[7]i64
 
     print("s1={any}, s2={any}, s3={any}\n", .{ s1, s2, s3 });
 }
 
-// This function is pretty wild because it executes at runtime
-// and is part of the final compiled program. The function is
-// compiled with unchanging data sizes and types.
+// 此函数非常疯狂，因为它在运行时执行，并且是最终编译的程序的一部分。该函数编译时具有不变的数据大小和类型。
 //
-// And yet it ALSO allows for different sizes and types. This
-// seems paradoxical. How could both things be true?
+// 然而，它也允许不同的大小和类型。这似乎是自相矛盾的。两件事怎么可能都是真的？
 //
-// To accomplish this, the Zig compiler actually generates a
-// separate copy of the function for every size/type combination!
-// So in this case, three different functions will be generated
-// for you, each with machine code that handles that specific
-// data size and type.
+// 为了实现这一点，Zig编译器实际上会为每个大小/类型组合生成单独的函数副本！
+// 因此，在这种情况下，将为您生成三个不同的函数，每个函数都具有处理该特定数据大小和类型的机器代码。
 //
-// Please fix this function so that the 'size' parameter:
+// 请修复此函数，以便“size”参数：
 //
-//     1) Is guaranteed to be known at compile time.
-//     2) Sets the size of the array of type T (which is the
-//        sequence we're creating and returning).
+//     1）保证在编译时已知。
+//     2）设置T类型数组（我们正在创建和返回的序列）的大小。
 //
-fn makeSequence(comptime T: type, ??? size: usize) [???]T {
-    var sequence: [???]T = undefined;
+fn makeSequence(comptime T: type, comptime size: usize) [size]T {
+    var sequence: [size]T = undefined;
     var i: usize = 0;
 
     while (i < size) : (i += 1) {
